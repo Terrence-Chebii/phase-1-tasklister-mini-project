8043673c34862fb3e833fdea0cf3e3c110fc42ed
bigman <terrence.chebii@student.moringaschool.com>
2023-03-24 23:19:08 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let x = document.querySelector('#create-task-form')
+  x.addEventListener('submit' , (e) => {
+    e.preventDefault()
+    toDo(e.target.description.value)
+    x.reset()
+  })
 });
+ 
+function toDo(task){
+  let btn = document.createElement('button')
+  let p = document.createElement('p')
+  btn.addEventListener('click' , Delete)
+  btn.textContent = 'x'
+  p.textContent = task
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+  console.log(toDo)
+}
+
+function Delete(e){
+  e.target.parentNode.remove()
+}
